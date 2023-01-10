5fe4a93d50a378ddeb0a1d0897d1c21ea4fcbfc0
Denio1212 <shehuden22@yahoo.com>
2023-01-10 05:36:45 -0800

    First

--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,14 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stddef.h>
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -21,6 +27,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -37,8 +44,4 @@ struct binary_tree_s
     struct binary_tree_s *right;
 };
 
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
 #endif
