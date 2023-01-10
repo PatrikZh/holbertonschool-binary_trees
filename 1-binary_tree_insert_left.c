859daad36e9370ed4d287f44754087d7b9f473ce
Denio1212 <shehuden22@yahoo.com>
2023-01-10 07:18:12 -0800

    Fuck you

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,24 +1,23 @@
 #include "binary_trees.h"
+
 /**
-* binary_tree_node - new node to add
-* @parent: parent node
-* @value: value assigned to its data
-* Return: new node
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *insert_left;
+	binary_tree_t *new;
 
-	insert_left = malloc(sizeof(binary_tree_t));
-	if (!parent || !insert_left)
+	new = malloc(sizeof(binary_tree_t));
+	if (!new || !parent)
 		return (NULL);
-	insert_left->n = value;
+	new->n = value;	
 	if (parent->left)
 	{
-		insert_left->left = parent->left;
-		parent->left->parent = insert_left;
-		insert_left->parent = parent;
+		parent->left->parent = new;
+		new->left = parent->left;
+		new->parent = parent;
 	}
-	parent->left = insert_left;
-	return (insert_left);
+	parent->left = new;
+
+	return (new);
+	
 }
