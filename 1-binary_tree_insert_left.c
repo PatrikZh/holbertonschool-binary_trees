ea7e1978a1875762c99af3ccedda3d021eea0c69
Denio1212 <shehuden22@yahoo.com>
2023-01-10 07:37:04 -0800

    lf

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,10 @@
 #include "binary_trees.h"
 
 /**
+* binary_tree_insert_left - new left
+* @parent: PAPA
+* @value: 1
+* Return: new
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
@@ -19,6 +23,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	parent->left = new;
 
-	return (new);
-	
+	return (new);	
 }
