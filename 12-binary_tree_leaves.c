d382b2057ef65fd9f84635cdb2f265c9fe385970
Denio1212 <shehuden22@yahoo.com>
2023-01-11 12:12:23 -0800

    Count leaf

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+* binary_tree_leaves - counts all leaves in a tree
+* @tree: ..
+* Return: what it does
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
+
